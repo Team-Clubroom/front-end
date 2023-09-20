be136de8f261a6e449b73b38d8940569365d0939
Luka Woodson <llwoodson@ualr.edu>
2023-09-20 02:00:45 -0500

    Add error page

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import SignupPage from "./pages/content/signup/signup.page.tsx";
 import AppMainPage from "./pages/content/app-main/app-main.page.tsx";
 import {NavbarComponent} from "./components/navbar/navbar.component.tsx";
 import FooterComponent from "./components/footer/footer.component.tsx";
+import ErrorPage from "./pages/misc/error/error.page.tsx";
 
 export default function App() {
     return (
@@ -19,6 +20,7 @@ export default function App() {
                 <Route path="/login" element={<LoginPage/>}/>
                 <Route path="/signup" element={<SignupPage/>}/>
                 <Route path="/appmain" element={<AppMainPage/>}/>
+                <Route path="*" element={<ErrorPage/>}/>
             </Routes>
             <FooterComponent page={'landing'}/>
         </BrowserRouter>
--- src/pages/misc/error/error.page.tsx
+++ src/pages/misc/error/error.page.tsx
@@ -1,6 +1,27 @@
+import {errorStyles} from "./error.page.styles.tsx";
+import {NavLink} from "react-router-dom";
+
 function ErrorPage() {
+    // TODO: get error number from state
+    const errorNumber: number = 404;
+
     return (
-        <div>Error Page</div>
+        <div className={errorStyles.pageContainer}>
+            <div className={errorStyles.contentContainer}>
+                <div className={errorStyles.errorContainer}>
+                    <div className={errorStyles.errorHeader}>
+                        <h1 className={errorStyles.errorNumber}>{errorNumber}</h1>
+                        <h1 className={errorStyles.errorTitle}>oops! Page not found</h1>
+                        <p className={errorStyles.errorText}>
+                            Oops! The page you are looking for does not exist. It might have been moved or deleted.
+                        </p>
+                        <NavLink to={"/"}>
+                            <button className={errorStyles.homeButton}>HOME</button>
+                        </NavLink>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
