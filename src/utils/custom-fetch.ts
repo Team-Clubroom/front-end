b993478473eec82a93b5afa685a26c4b94f378d6
Dako Albeik <dako.albeik@gmail.com>
2023-10-01 16:35:17 -0500

    Refactor custom fetch and add fetching employers

--- src/utils/custom-fetch.ts
+++ src/utils/custom-fetch.ts
@@ -3,31 +3,32 @@ import { SuccessResponse } from "../models/api.types.ts";
 export enum ApiRoutes {
   Register = "register",
   Login = "login",
+  Employers = "employers",
 }
 
 export const customFetch = async <T = undefined>(
-    apiRoute: ApiRoutes,
-    method: "GET" | "POST",
-    body: Record<string, string>,
+  apiRoute: ApiRoutes,
+  method: "GET" | "POST",
+  body?: Record<string, string>,
 ): Promise<SuccessResponse<T>> => {
-    const response = await fetch(`/api/${apiRoute}`, {
-        headers: {
-            "Content-type": "application/json",
-        },
-        method,
-        body: JSON.stringify(body),
-    });
+  const response = await fetch(`/api/${apiRoute}`, {
+    headers: {
+      "Content-type": "application/json",
+    },
+    method,
+    ...(body ? { body: JSON.stringify(body) } : {}),
+  });
 
-    const result = await response.json();
-    if (response.ok) {
-        return result as SuccessResponse<T>;
+  const result = await response.json();
+  if (response.ok) {
+    return result as SuccessResponse<T>;
+  } else {
+    if ("error" in result) {
+      throw Error(result.error);
     } else {
-        if ("error" in result) {
-            throw Error(result.error);
-        } else {
-            throw Error(
-                `${apiRoute} request failed with status code ${response.status}`,
-            );
-        }
+      throw Error(
+        `${apiRoute} request failed with status code ${response.status}`,
+      );
     }
+  }
 };
