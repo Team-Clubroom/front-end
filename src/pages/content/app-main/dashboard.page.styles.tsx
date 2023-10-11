71adc420cbca19094c1b235549def7cee2d93c31
Luka Woodson <llwoodson@ualr.edu>
2023-10-10 19:58:24 -0500

    Add side bar for dashboard

---
+++ src/pages/content/app-main/dashboard.page.styles.tsx
@@ -0,0 +1,16 @@
+export const dashboardPageStrings = {
+  app_main: "w-screen h-screen flex flex-col items-center justify-center",
+  view: "w-screen flex flex-row",
+  //view: "w-full h-full flex flex-row items-center justify-center bg-cool-gray-100",
+  sidenavButton:
+    "p-2 border-2 bg-white rounded-md border-gray-200 shadow-lg text-gray-500 focus:bg-teal-500 focus:outline-none focus:text-white absolute top-0 left-0 sm:hidden",
+  sidebar:
+    "bg-white h-full md:block shadow-xl px-3 w-30 md:w-60 lg:w-60 overflow-x-hidden transition-transform duration-300" +
+    " ease-in-out",
+  profile: "space-y-3",
+  searchInput:
+    "flex border-2 border-gray-200 rounded-md focus-within:ring-2 ring-teal-500",
+  menuLink:
+    "text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:text-base rounded-md transition duration-150 ease-in-out",
+  mainContent: "h-screen w-full flex",
+};
--- src/pages/content/app-main/dashboard.page.tsx
+++ src/pages/content/app-main/dashboard.page.tsx
@@ -1,13 +1,41 @@
 import { useAuthContext } from "../../../contexts/auth/auth.context.tsx";
+import { dashboardPageStrings } from "./dashboard.page.styles.tsx";
 
 function DashboardPage() {
   const user = useAuthContext();
   return (
-    <div>
-      <h1>Private dashboard</h1>
-      <p>Welcome {user.email}</p>
+    // <div className={dashboardPageStrings.app_main}>
+    <div className={dashboardPageStrings.view}>
+      <button className={dashboardPageStrings.sidenavButton}>
+        <svg
+          className="w-5 h-5 fill-current"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          {/* ... SVG Path for the sidenav button */}
+        </svg>
+      </button>
+      <div className={dashboardPageStrings.sidebar}>
+        {/* Sidebar content */}
+      </div>
+      <div className={dashboardPageStrings.mainContent}>
+        {/* Main content */}
+        <h1>Private dashboard</h1>
+        <p>Welcome {user.email}</p>
+      </div>
     </div>
+    // </div>
   );
 }
 
 export default DashboardPage;
+
+/*
+ *
+ * <div>
+ *   <h1>Private dashboard</h1>
+ *   <p>Welcome {user.email}</p>
+ * </div>
+ *
+ * */
