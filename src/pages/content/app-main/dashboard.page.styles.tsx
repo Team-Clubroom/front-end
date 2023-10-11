7b48447bca2c84dafe46c6ab1b605e59b04a1c34
Luka Woodson <llwoodson@ualr.edu>
2023-10-10 22:17:31 -0500

    Add in basic side menu content

--- src/pages/content/app-main/dashboard.page.styles.tsx
+++ src/pages/content/app-main/dashboard.page.styles.tsx
@@ -1,13 +1,16 @@
-export const dashboardPageStrings = {
+export const dashboardStyles = {
   view: "w-screen flex flex-row",
   sidenavButton:
     "p-2 border-2 bg-white rounded-md border-gray-200 shadow-lg text-gray-500 focus:bg-teal-500 focus:outline-none focus:text-white absolute top-0 left-0 sm:hidden",
   sidebar:
     "bg-white h-full md:block shadow-xl px-3 w-30 md:w-60 lg:w-60 overflow-x-hidden transition-transform duration-300" +
     " ease-in-out",
-  profile: "space-y-3",
-  searchInput:
-    "flex border-2 border-gray-200 rounded-md focus-within:ring-2 ring-teal-500",
+  profile: "flex flex-col gap-2 mt-8 mb-8",
+  profile_name: "font-medium text-xs md:text-sm text-center text-teal-500",
+  access_level: "text-xs text-gray-500 text-center",
+  // searchInput:
+  //   "flex border-2 border-gray-200 rounded-md focus-within:ring-2 ring-teal-500",
+  menu: "flex flex-col space-y-2",
   menuLink:
     "text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:text-base rounded-md transition duration-150 ease-in-out",
   mainContent: "h-screen w-full flex",
--- src/pages/content/app-main/dashboard.page.tsx
+++ src/pages/content/app-main/dashboard.page.tsx
@@ -1,25 +1,124 @@
 import { useAuthContext } from "../../../contexts/auth/auth.context.tsx";
-import { dashboardPageStrings } from "./dashboard.page.styles.tsx";
+import { dashboardStyles } from "./dashboard.page.styles.tsx";
 
 function DashboardPage() {
   const user = useAuthContext();
   return (
-    <div className={dashboardPageStrings.view}>
-      <button className={dashboardPageStrings.sidenavButton}>
-        <svg
-          className="w-5 h-5 fill-current"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          {/* ... SVG Path for the sidenav button */}
-        </svg>
-      </button>
-      <div className={dashboardPageStrings.sidebar}>
-        {/* Sidebar content */}
+    <div className={dashboardStyles.view}>
+      <div className={dashboardStyles.sidebar}>
+        <div id="profile" className={dashboardStyles.profile}>
+          {/*  // TODO: Change later to be name instead of email */}
+          <h2 className={dashboardStyles.profile_name}>{user.email}</h2>
+          {/*  // TODO: Change later to be actual access level */}
+          <p className={dashboardStyles.access_level}>Administrator</p>
+        </div>
+        <div id="menu" className={dashboardStyles.menu}>
+          <a href="" className={dashboardStyles.menuLink}>
+            <svg
+              className="w-6 h-6 fill-current inline-block"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
+            </svg>
+            <span className="">Dashboard</span>
+          </a>
+          <a href="" className={dashboardStyles.menuLink}>
+            <svg
+              className="w-6 h-6 fill-current inline-block"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z"></path>
+            </svg>
+            <span className="">Products</span>
+          </a>
+          <a href="" className={dashboardStyles.menuLink}>
+            <svg
+              className="w-6 h-6 fill-current inline-block"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
+              <path
+                fillRule="evenodd"
+                d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <span className="">Reports</span>
+          </a>
+          <a href="" className={dashboardStyles.menuLink}>
+            <svg
+              className="w-6 h-6 fill-current inline-block"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z"></path>
+              <path d="M15 7v2a4 4 0 01-4 4H9.828l-1.766 1.767c.28.149.599.233.938.233h2l3 3v-3h2a2 2 0 002-2V9a2 2 0 00-2-2h-1z"></path>
+            </svg>
+            <span className="">Messages</span>
+          </a>
+          <a href="" className={dashboardStyles.menuLink}>
+            <svg
+              className="w-6 h-6 fill-current inline-block"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <span className="">Calendar</span>
+          </a>
+          <a href="" className={dashboardStyles.menuLink}>
+            <svg
+              className="w-6 h-6 fill-current inline-block"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <span className="">Table</span>
+          </a>
+          <a href="" className={dashboardStyles.menuLink}>
+            <svg
+              className="w-6 h-6 fill-current inline-block"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM14 11a1 1 0 011 1v1h1a1 1 0 110 2h-1v1a1 1 0 11-2 0v-1h-1a1 1 0 110-2h1v-1a1 1 0 011-1z"></path>
+            </svg>
+            <span className="">UI Components</span>
+          </a>
+          <a href="" className={dashboardStyles.menuLink}>
+            <svg
+              className="w-6 h-6 fill-current inline-block"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"></path>
+            </svg>
+            <span className="">Users</span>
+          </a>
+        </div>
       </div>
-      <div className={dashboardPageStrings.mainContent}>
-        {/* Main content */}
+      <div className={dashboardStyles.mainContent}>
+        {/* ------------------- PUT MAIN CONTENT HERE ------------------- */}
         <div>
           <h1>Private dashboard</h1>
           <p>Welcome {user.email}</p>
