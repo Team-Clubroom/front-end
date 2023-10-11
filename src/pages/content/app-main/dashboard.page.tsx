c2cef86ee53b1e0294f66f5028bf4ffe5267b585
Luka Woodson <llwoodson@ualr.edu>
2023-10-10 23:28:17 -0500

    Install VISX Responsive and make graph take up available space in the parent

--- src/pages/content/app-main/dashboard.page.tsx
+++ src/pages/content/app-main/dashboard.page.tsx
@@ -2,6 +2,8 @@ import { useAuthContext } from "../../../contexts/auth/auth.context.tsx";
 import { useEffect, useState } from "react";
 import { ApiRoutes, customFetch } from "../../../utils/custom-fetch.ts";
 import { Employer } from "../../../models/employer.types.ts";
+import ParentSize from "@visx/responsive/lib/components/ParentSize";
+import TreeComponent from "../../../components/tree/tree.component.tsx";
 
 function DashboardPage() {
   const user = useAuthContext();
@@ -24,16 +26,19 @@ function DashboardPage() {
       });
   }, []);
   return (
-    <div>
-      <h1>Private dashboard</h1>
-      <p>Welcome {user.email}</p>
-      <div>
-        <span>Employers:</span>
-        {employers.map((employer) => {
-          return <div key={employer.id}>{employer.name}</div>;
-        })}
-      </div>
-    </div>
+    // <div>
+    //   <h1>Private dashboard</h1>
+    //   <p>Welcome {user.email}</p>
+    //   <div>
+    //     <span>Employers:</span>
+    //     {employers.map((employer) => {
+    //       return <div key={employer.id}>{employer.name}</div>;
+    //     })}
+    //   </div>
+    // </div>
+    <ParentSize>
+      {({ width, height }) => <TreeComponent width={width} height={height} />}
+    </ParentSize>
   );
 }
 
