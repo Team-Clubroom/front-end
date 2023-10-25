f95543ec0f3d4e9f45a066315e025eba5cd05e53
cory-eheart-1 <cory.eheart@gmail.com>
2023-10-25 14:18:07 -0500

    added titles to error and maitenance

--- src/pages/misc/error/error.page.tsx
+++ src/pages/misc/error/error.page.tsx
@@ -1,28 +1,34 @@
 import { errorStyles } from "./error.page.styles.tsx";
 import { NavLink } from "react-router-dom";
+import { Helmet } from 'react-helmet';
 
 function ErrorPage() {
   // TODO: get error number from state
   const errorNumber: number = 404;
 
   return (
-    <div className={errorStyles.pageContainer}>
-      <div className={errorStyles.contentContainer}>
-        <div className={errorStyles.errorContainer}>
-          <div className={errorStyles.errorHeader}>
-            <h1 className={errorStyles.errorNumber}>{errorNumber}</h1>
-            <h1 className={errorStyles.errorTitle}>oops! Page not found</h1>
-            <p className={errorStyles.errorText}>
-              Oops! The page you are looking for does not exist. It might have
-              been moved or deleted.
-            </p>
-            <NavLink to={"/"}>
-              <button className={errorStyles.homeButton}>HOME</button>
-            </NavLink>
+    <>
+      <Helmet>
+        <title>Page Not Found - CELDV</title>
+      </Helmet>
+      <div className={errorStyles.pageContainer}>
+        <div className={errorStyles.contentContainer}>
+          <div className={errorStyles.errorContainer}>
+            <div className={errorStyles.errorHeader}>
+              <h1 className={errorStyles.errorNumber}>{errorNumber}</h1>
+              <h1 className={errorStyles.errorTitle}>oops! Page not found</h1>
+              <p className={errorStyles.errorText}>
+                Oops! The page you are looking for does not exist. It might have
+                been moved or deleted.
+              </p>
+              <NavLink to={"/"}>
+                <button className={errorStyles.homeButton}>HOME</button>
+              </NavLink>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
