07382b22cf875d23d8d0226724daad08bd565249
Dako Albeik <dako.albeik@gmail.com>
2023-10-30 20:22:49 -0500

    Attempt 02 to fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,17 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
   return {
     server: {
       proxy: {
         "/api": {
           // TODO: load this url from .env
-          target:
-            mode === "development"
-              ? "http://127.0.0.1:5000"
-              : "http://capstone-api-env-01.eba-cp4z6h2m.us-east-2.elasticbeanstalk.com",
+          target: isDev
+            ? "http://127.0.0.1:5000"
+            : "http://capstone-api-env-01.eba-cp4z6h2m.us-east-2.elasticbeanstalk.com",
           changeOrigin: true,
+          secure: false,
           rewrite: (path) => path.replace(/^\/api/, ""),
         },
       },
