0d12664107e307338a4e56d1503a14bcf272fd8d
Dako Albeik <dako.albeik@gmail.com>
2023-11-03 16:27:41 -0500

    Create custom search input

---
+++ src/components/search-box/search-box.component.tsx
@@ -0,0 +1,45 @@
+import React, { useRef } from "react";
+
+interface SearchBoxProps {
+  placeholder: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  onSearch?: (value: string) => void;
+}
+
+function SearchBoxComponent({
+  placeholder,
+  onSearch,
+  onChange,
+}: SearchBoxProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <div className={"relative"}>
+      <span
+        className={`material-symbols-outlined absolute translate-y-1/2 bottom-1/2 pl-2 text-white`}
+      >
+        device_hub
+      </span>
+      <input
+        type={"text"}
+        ref={inputRef}
+        placeholder={placeholder}
+        onChange={onChange}
+        className={
+          "outline-0 py-2.5 rounded-full border focus:bg-gray-800 bg-gray-500 text-white transition-colors w-80"
+        }
+        style={{ paddingInline: "36px" }}
+      />
+      <button
+        onClick={() => onSearch && onSearch(inputRef.current?.value || "")}
+        className={
+          "absolute translate-y-1/2 bottom-1/2 right-0 pr-2 flex border-0 outline-0"
+        }
+      >
+        <span className={`material-symbols-outlined text-white`}>search</span>
+      </button>
+    </div>
+  );
+}
+
+export default SearchBoxComponent;
