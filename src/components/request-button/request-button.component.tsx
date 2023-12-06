aa6e26da686f5b235522af590eb6d6725b1adce7
cory-eheart-1 <cory.eheart@gmail.com>
2023-12-06 16:33:36 -0600

    made requestButton confirmCode p into span

--- src/components/request-button/request-button.component.tsx
+++ src/components/request-button/request-button.component.tsx
@@ -58,7 +58,7 @@ export const RequestButtonComponent = ({
           d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
         />
       </svg>
-      <p>{success?.text}</p>
+      <span>{success?.text}</span>
     </>
   );
 
